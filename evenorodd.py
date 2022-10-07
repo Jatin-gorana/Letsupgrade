e392caca984dc6765e5c69c77da23582105e5361
Jatin-gorana <105877878+Jatin-gorana@users.noreply.github.com>
2022-10-07 22:19:54 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,9 @@
+# This program explains how to know even or odd number using python
+print("Enter a number to find if it is even or odd")
+num = float(input())
+
+if num % 2 == 0:
+    print(num, "is even number")
+else:
+    print(num, "is odd number")
+
